c81bd7349ef72c4a9cd62fbc69be898aeda2854a
Kevin Colley <kjcolley7@gmail.com>
2018-04-20 11:24:36 -0400

    Revert "Pin specific package versions to hotfix Routing 3.0.0 breaking changes"
    This reverts commit a3f58c95037f0822ef3ad251914762869dc80355.

--- Package.swift
+++ Package.swift
@@ -5,22 +5,19 @@ let package = Package(
 	name: "MentorMe",
 	dependencies: [
 		// 💧 A server-side Swift web framework.
-		.package(url: "https://github.com/vapor/vapor.git", .exact("3.0.0-rc.2.6")),
+		.package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2.6"),
 		
 		// 🖋🐬 Swift ORM (queries, models, relations, etc) built on MySQL.
-		.package(url: "https://github.com/vapor/fluent-mysql.git", .exact("3.0.0-rc.2.3")),
+		.package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.2.3"),
 		
 		// 🍃 An expressive, performant, and extensible templating language built for Swift.
-		.package(url: "https://github.com/vapor/leaf.git", .exact("3.0.0-rc.2")),
+		.package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2"),
 		
 		// 👤 Authentication and Authorization layer for Fluent.
-		.package(url: "https://github.com/vapor/auth.git", .exact("2.0.0-rc.3.1")),
+		.package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc.3.1"),
 		
 		// ⚡️Non-blocking, event-driven Redis client.
-		.package(url: "https://github.com/vapor/redis.git", .exact("3.0.0-rc.2.3")),
-		
-		// 🚍 High-performance trie-node router.
-		.package(url: "https://github.com/vapor/routing.git", .exact("3.0.0-rc.2"))
+		.package(url: "https://github.com/vapor/redis.git", from: "3.0.0-rc.2.3")
 	],
 	targets: [
 		.target(name: "App", dependencies: [
